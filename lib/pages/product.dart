547ea23e275dfa98de03533cb98ed16dffefb55f
Feng Zhu <feng.zhu1980@gmail.com>
2018-11-13 12:15:28 +1300

    Reganized pages.

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -1,34 +1,65 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import '../widgets/ui_elements/title_default.dart';
 
 class ProductPage extends StatelessWidget {
-  final String _title;
-  final String _imageUrl;
-  final String _description;
-  final double _price;
+  final String title;
+  final String imageUrl;
+  final String description;
+  final double price;
 
-  ProductPage(this._title, this._imageUrl, this._description, this._price);
+  ProductPage(this.title, this.imageUrl, this.description, this.price);
 
   _showWarningDialog(BuildContext context) {
     showDialog(
-      context: context,
-      builder: (BuildContext context) {
-      return AlertDialog(
-        title: Text('Are you sure?'),
-        content: Text('This action cannot be undone!'),
-        actions: <Widget>[
-          FlatButton(child: Text('DISCARD'), onPressed: () {
-            Navigator.pop(context);
-          },),
-          FlatButton(child: Text('CONTINUE'), onPressed: () {
-            Navigator.pop(context);
-            Navigator.pop(context, true);
-          },),
-        ],
-      );
-    });
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Are you sure?'),
+            content: Text('This action cannot be undone!'),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('DISCARD'),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+              FlatButton(
+                child: Text('CONTINUE'),
+                onPressed: () {
+                  Navigator.pop(context);
+                  Navigator.pop(context, true);
+                },
+              ),
+            ],
+          );
+        });
   }
+
+  Widget _buildAddressPriceRow() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          'Union Square, San Francisco',
+          style: TextStyle(fontFamily: 'Oswald', color: Colors.grey),
+        ),
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 5.0),
+          child: Text(
+            '|',
+            style: TextStyle(color: Colors.grey),
+          ),
+        ),
+        Text(
+          '\$' + price.toString(),
+          style: TextStyle(fontFamily: 'Oswald', color: Colors.grey),
+        )
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
@@ -38,77 +69,38 @@ class ProductPage extends StatelessWidget {
         return Future.value(false);
       },
       child: Scaffold(
-        appBar: AppBar(
-          title: Text(_title),
-        ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Image.asset(_imageUrl),
-            // Title and price
-            Container(
-              padding: EdgeInsets.all(10.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    _title,
-                    style: TextStyle(
-                      fontSize: 30.0,
-                      fontWeight: FontWeight.w400,
-                    ),
-                  ),
-                  SizedBox(
-                    width: 8.0,
-                  ),
-                  Container(
-                    padding: EdgeInsets.symmetric(horizontal: 6, vertical: 3),
-                    decoration: BoxDecoration(
-                      color: Colors.purple,
-                      borderRadius: BorderRadius.circular(5.0)
-                    ),
-                    child: Text(
-                      '\$${_price.toString()}',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  )
-                ],
+          appBar: AppBar(
+            title: Text(title),
+          ),
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Image.asset(imageUrl),
+              // Title and price
+              Container(
+                padding: EdgeInsets.all(10.0),
+                child: TitleDefault(title),
               ),
-            ),
-            // Address
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 6.0, vertical: 2.5),
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.grey,
-                  width: 1.0
-                ),
-                borderRadius: BorderRadius.circular(6.0),
-              ),
-              child: Text('Union Square, San Francisco'),
-            ),
-            // Description
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.5),
-              child: Text(
-                _description,
-                style: TextStyle(
-                  fontSize: 25,
-                  fontWeight: FontWeight.normal,
-                  fontFamily: 'Oswald'
+              _buildAddressPriceRow(),
+              // Description
+              Container(
+                padding: EdgeInsets.all(10.0),
+                child: Text(
+                  description,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontWeight: FontWeight.normal, fontFamily: 'Oswald'),
                 ),
               ),
-            ),
-            Container(
-              child: RaisedButton(
-                color: Theme.of(context).accentColor,
-                child: Text('DELETE'),
-                onPressed: () => _showWarningDialog(context),
-              ),
-            )
-          ],
-        )
-      ),
+              Container(
+                child: RaisedButton(
+                  color: Theme.of(context).accentColor,
+                  child: Text('DELETE'),
+                  onPressed: () => _showWarningDialog(context),
+                ),
+              )
+            ],
+          )),
     );
   }
 }
